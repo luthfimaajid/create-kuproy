2d54bca5c8daefedf67051e2212a20e79af4d76d
temmy76 <temmy.mahesa.tif420@polban.ac.id>
2022-11-09 20:22:46 +0700

    add fe vue

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import User from "./components/User.vue";
+// import addUser from "./components/AddUser.vue";
+</script>
+
+<template>
+	<div class="container">
+		<User />
+		<!-- <addUser /> -->
+	</div>
+</template>
+
+<style scoped>
+.container {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+</style>
