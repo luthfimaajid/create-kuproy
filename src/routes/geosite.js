40a186005959c013f3d138576a6d202bf112c1b4
dizaam <luthfimaajid@gmail.com>
2022-11-23 18:16:08 +0700

    blank be project

--- src/routes/geosite.js
+++ src/routes/geosite.js
@@ -3,10 +3,4 @@ const geositeController = require('../controllers/geosite');
 
 const routes = express.Router();
 
-routes.get("/:id", geositeController.get);
-routes.get("/", geositeController.getAll);
-routes.post("/", geositeController.create);
-routes.patch("/:id", geositeController.update);
-routes.delete("/:id", geositeController.delete);
-
 module.exports = routes;
