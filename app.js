5fd75617fbd07857a2d40802f66122672f576664
dizaam <luthfimaajid@gmail.com>
2022-11-23 21:25:32 +0700

    blank postgres be

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const cors = require("cors");
 
 require("dotenv").config();
 
-const siteRoute = require("./src/routes/geosite");
-const areaRoute = require("./src/routes/geoarea");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -14,8 +12,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan("dev"));
-app.use("/site", siteRoute);
-app.use("/area", areaRoute);
 
 app.get("/", (req, res) => {
 	res.send("get");
