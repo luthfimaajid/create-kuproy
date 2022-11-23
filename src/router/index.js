a9fdcc151b9fa33d46bf760ea184881daae45466
dizaam <luthfimaajid@gmail.com>
2022-11-23 18:18:29 +0700

    blank fe project

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
-import GeoArea from "../views/GeoArea.vue";
 
 const routes = [
 	{
 		path: "/",
 		name: "Home",
 		component: Home,
-	},
-	{
-		path: "/geoarea",
-		name: "GeoArea",
-		component: GeoArea,
-	},
+	}
 ];
 
 let router = createRouter({
