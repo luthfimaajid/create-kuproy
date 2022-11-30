33bbb7ad47798ccbf8dcf4605634587f84dca750
temmy76 <temmy.mahesa.tif420@polban.ac.id>
2022-11-30 22:10:20 +0700

    update dependency

---
+++ src/components/Lorem.vue
@@ -0,0 +1,10 @@
+<template>
+	<div>
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+			asperiores, voluptates, quod, voluptas quia voluptatem quae accusantium
+			quibusdam quidem voluptatum quos. Quisquam, quae accusantium. Quisquam,
+			quae accusantium. Quisquam, quae accusantium.
+		</p>
+	</div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,8 @@
-<script setup></script>
+<script setup>
+import LoremVue from '../components/Lorem.vue';
+</script>
 
 <template>
 	<h1>Home</h1>
-	<div class="container d-flex justify-content-center align-items-center">
-		<p>
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-			asperiores, voluptates, quod, voluptas quia voluptatem quae accusantium
-			quibusdam quidem voluptatum quos. Quisquam, quae accusantium. Quisquam,
-			quae accusantium. Quisquam, quae accusantium.
-		</p>
-	</div>
+	<LoremVue />
 </template>
