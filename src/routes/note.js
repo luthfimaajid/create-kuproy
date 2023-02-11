f7aff623c9208c3ac1787395bdd87a2abb2a099d
dizaam <luthfimaajid@gmail.com>
2023-02-11 10:08:58 +0700

    refine express postgres

---
+++ src/routes/note.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const noteController = require('../controllers/note');
+
+const routes = express.Router();
+
+routes.get("/:id", noteController.get);
+routes.get("/", noteController.getAll);
+routes.post("/", noteController.create);
+routes.patch("/:id", noteController.update);
+routes.delete("/:id", noteController.delete);
+
+
+module.exports = routes
