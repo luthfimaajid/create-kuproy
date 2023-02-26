3a97f9bfa12572b1cac5c2e591cf1e2940c7fd82
dizaam <luthfimaajid@gmail.com>
2023-02-26 18:56:11 +0700

    blank

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from "vue";
-import "bootstrap/dist/css/bootstrap.css";
-import "./style.css";
-import router from "./router";
-import App from "./App.vue";
-
-createApp(App).use(router).mount("#app");
-
-import "bootstrap/dist/js/bootstrap.js";
