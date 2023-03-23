0daea3f1274c2d7faabea15e76ceaa105e10f859
dizaam <luthfimaajid@gmail.com>
2023-03-23 10:03:42 +0700

    add cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const fastify = require('fastify')({
   logger: true
 })
+const cors = require('@fastify/cors');
 
 require('dotenv').config();
 const mongoose = require("mongoose");
@@ -9,6 +10,10 @@ mongoose.connect(process.env.MONGO_URI)
 	.catch(error => console.error(error));
 
 fastify.register(require("./src/routes/note"), {prefix: "/api/note"});
+fastify.register(cors, {
+  origin: '*',
+  methods: ["POST", "GET", "PUT", "PATCH", "DELETE"],
+})
 
 const start = async () => {
   try {
